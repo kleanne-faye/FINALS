af98bc5770e2fa956b7513abf8114a34aceaa08c
kleanne-faye <kleannelauro@gmail.com>
2022-10-21 16:44:15 +0800

    updated_registration

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome to the University of the Cordilleras! <br> Your form is successfully submitted!
+ <?php echo $_POST["user_dob"]; ?><br>
+Your email address is: <?php echo $_POST["EmailID"]; ?>
+
+</body>
+</html>
